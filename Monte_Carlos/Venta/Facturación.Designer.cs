43adcaabefefaaa64fb981e68a983d77bcfd5148
wilmergarcia7 <wilmergarciap1@gmail.com>
2021-10-27 19:24:38 -0600

    Se agrego la pantalla Facturación
    Se agrego la pantalla encargada de facturar los productos que anteriormente estaban en lista de espera.

---
+++ Monte_Carlos/Venta/Facturación.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Monte_Carlos.Venta
+{
+    partial class Facturación
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgListadeProductos = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtInsertarCliente = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgListadeProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(156, 56);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(299, 30);
+            this.lblNombre.TabIndex = 46;
+            this.lblNombre.Text = "Facturación de venta";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 30);
+            this.label1.TabIndex = 47;
+            this.label1.Text = "Cliente: ";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 30);
+            this.label2.TabIndex = 48;
+            this.label2.Text = "Yovany Hernández";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(156, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 30);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Lista de productos";
+            // 
+            // dgListadeProductos
+            // 
+            this.dgListadeProductos.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dgListadeProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgListadeProductos.Location = new System.Drawing.Point(161, 225);
+            this.dgListadeProductos.Name = "dgListadeProductos";
+            this.dgListadeProductos.RowHeadersWidth = 51;
+            this.dgListadeProductos.RowTemplate.Height = 24;
+            this.dgListadeProductos.Size = new System.Drawing.Size(857, 184);
+            this.dgListadeProductos.TabIndex = 50;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(156, 438);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 30);
+            this.label5.TabIndex = 52;
+            this.label5.Text = "ISV (12%):";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(156, 480);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 30);
+            this.label6.TabIndex = 53;
+            this.label6.Text = "Subtotal:";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(156, 526);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 30);
+            this.label7.TabIndex = 54;
+            this.label7.Text = "Total:";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(313, 438);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 30);
+            this.label8.TabIndex = 55;
+            this.label8.Text = "L. 0.00";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(313, 483);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 30);
+            this.label9.TabIndex = 56;
+            this.label9.Text = "L. 0.00";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(313, 529);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 30);
+            this.label10.TabIndex = 57;
+            this.label10.Text = "L. 0.00";
+            // 
+            // txtInsertarCliente
+            // 
+            this.txtInsertarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(205)))), ((int)(((byte)(51)))));
+            this.txtInsertarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInsertarCliente.ForeColor = System.Drawing.Color.Black;
+            this.txtInsertarCliente.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.txtInsertarCliente.Location = new System.Drawing.Point(161, 580);
+            this.txtInsertarCliente.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInsertarCliente.Name = "txtInsertarCliente";
+            this.txtInsertarCliente.Size = new System.Drawing.Size(294, 46);
+            this.txtInsertarCliente.TabIndex = 58;
+            this.txtInsertarCliente.Text = "Confirmar venta";
+            this.txtInsertarCliente.UseVisualStyleBackColor = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(843, 56);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(175, 34);
+            this.dateTimePicker1.TabIndex = 59;
+            // 
+            // Facturación
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1203, 704);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtInsertarCliente);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgListadeProductos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Facturación";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "c";
+            ((System.ComponentModel.ISupportInitialize)(this.dgListadeProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgListadeProductos;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button txtInsertarCliente;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
